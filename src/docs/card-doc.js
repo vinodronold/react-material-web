929bfff049a2b78d460eded99ea0a0adfd8808af
vinodronold <vinodronoldbe@gmail.com>
2017-04-11 16:56:57 -0400

    Update Card Doc

--- src/docs/card-doc.js
+++ src/docs/card-doc.js
@@ -13,16 +13,15 @@ const CardDoc = () => {
             <h3>Cards</h3>
             <section style={styles.container}>
                 <Card style={styles.item}>
-                    <CardHeader
-                        title={'Cards Title'}
-                        subtitle={'Basic Cards'}
-                        avatar={'https://dummyimage.com/100x100/bdbdbd/222&text=A'}
-                    />
+                    <CardHeader>
+                        <CardTitle large> Basic Card </CardTitle>
+                        <CardSubTitle> Some subtitle </CardSubTitle>
+                    </CardHeader>
                     <CardText>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                             nisi ut aliquip ex ea commodo consequat.
-                        </CardText>
+                    </CardText>
                     <CardActions>
                         <Button raised primary compact>Action 1</Button>
                         <Button raised primary accent>Action 2</Button>
@@ -30,16 +29,17 @@ const CardDoc = () => {
                     </CardActions>
                 </Card>
                 <Card style={styles.item}>
-                    <CardHeader>
-                        <CardTitle large> Cards Title </CardTitle>
-                        <CardSubTitle> Cards with Verital Button </CardSubTitle>
-                    </CardHeader>
+                    <CardHeader
+                        title={'Avatar Cards'}
+                        subtitle={'Some subtitle'}
+                        avatar={'https://dummyimage.com/100x100/bdbdbd/222&text=A'}
+                    />
                     <CardText>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                             nisi ut aliquip ex ea commodo consequat.
-                        </CardText>
-                    <CardActions vertical>
+                    </CardText>
+                    <CardActions>
                         <Button raised primary compact>Action 1</Button>
                         <Button raised primary accent>Action 2</Button>
                         <Button raised>Action 3</Button>
